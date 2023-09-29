15b29b25b01ec334b1ad281fe4a3b531ac8207df
Serg Skol <sps676@gmail.com>
2023-09-29 14:59:27 +0200

    Handle add / remove on click

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # brick-game
-Flying bricks
+Flying bricks game. Press on empty background to add new brick, press on the brick to remove.
+
+How to run:
+'python brick-game.py"
--- main.py
+++ main.py
@@ -1,21 +1,31 @@
+# Press on empty background to add new brick, press on the brick to remove
+
 import pygame
 
 class Brick: 
   def __init__(self, x, y) -> None:
-    self.img = pygame.Surface(BRICK_SIZE)
+    self.img = pygame.Surface( (BRICK_SIZE, BRICK_SIZE) )
     self.img.fill(BRICK_COLOR)
     self.rect = self.img.get_rect()
     self.rect = self.rect.move( [x, y])
     self.speed = [1, 1]
-  
+
+def find_brick(bricks, x, y) :
+  index = -1
+  for brick in bricks :
+    index += 1
+    if (x > brick.rect.left and x < brick.rect.right) and (y > brick.rect.top and y < brick.rect.bottom) :
+      return index
+  return -1
+    
 pygame.init()
 
 HEIGHT = 640
 WIDTH = 1280
 BACKGROUND_COLOR = (0, 100, 100)
 BRICK_COLOR = (255, 255, 255)
-BRICK_SIZE = (20, 20)
-FPS = 240
+BRICK_SIZE = 80
+FPS = 100
 
 main_display = pygame.display.set_mode( (WIDTH, HEIGHT) )
 main_display.fill( BACKGROUND_COLOR )
@@ -28,14 +38,23 @@ bricks.append(new_brick)
 playing = True
 while playing :
   main_display_fps.tick(FPS)
+  
   for event in pygame.event.get() :
     if event.type == pygame.QUIT :
       playing = False
     elif event.type == pygame.MOUSEBUTTONDOWN:
       pos=pygame.mouse.get_pos()
       btn=pygame.mouse
-      new_brick = Brick(pos[0], pos[1])
-      bricks.append(new_brick)
+      
+      index = find_brick(bricks, pos[0], pos[1])
+      
+      if index == -1: #add new
+        new_brick = Brick(pos[0], pos[1])
+        bricks.append(new_brick)
+        FPS -= 10
+      else : # remove
+        bricks.pop(index)
+        FPS += 10
   
   main_display.fill(BACKGROUND_COLOR)
   
